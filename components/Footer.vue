dc90a661c29fabeefb608f49d335b505fe0e9624
anon <anon@debian-BULLSEYE-live-builder-AMD64>
2022-09-27 12:26:45 +0300

    dev: esab section

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mx-auto py-1 pt-1 px-4 lg:max-w-7xl lg:px-8">
     <footer class="p-4 bg-white rounded-sm shadow md:flex md:items-center md:justify-between md:p-6 border dark:bg-gray-800 dark:border-gray-700">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Главный сварщик</a>
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">Главный сварщик</a>
       </span>
       <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
--- components/sections/ESAB.vue
+++ components/sections/ESAB.vue
@@ -1,5 +1,47 @@
 <template>
   <section id="" class="mt-4">
+
+    <div class="mx-auto py-2 px-4 lg:max-w-7xl lg:px-8">
+
+      
+      <div class="grid md:grid-cols-2 gap-4">
+        <div>
+          <img 
+            src="http://127.0.0.1:8000/files/c/esab-banner-one.jpg" 
+          />
+        </div>
+        <div class="">
+          <img 
+            src="http://127.0.0.1:8000/files/c/handy-esab.jpg"
+          />
+        </div>
+      </div>
+
+
+
+      <div class="flex items-center">
+
+        <div class="hidden md:block flex-none">
+          <img
+            src="esab-logo.jpg"
+            width="220px"
+          />
+        </div>
+
+        <div class="flex-initial p-4">
+          <div class="">
+            <p class="text-2xl">Мы являемся официальным дистрибьютором-партнером концерна ESAB и официальным Авторизованным Сервисным Центром концерна ESAB</p>
+          </div>
+          <div class="">
+            <p class="text-sm">Бренд ESAB - синоним передового опыта в таких областях как: оборудование для ручной сварки и резки, сварочные материалы, автоматизированная сварка, механические системы резки. В каждой из этих сфер ESAB использует постоянно совершенствуемые методы, материалы и ноу-хау, чтобы в полной мере удовлетворить потребности всех секторов промышленности. Компания ESAB разрабатывает высокопроизводительные решения, которые отвечают требованиям заказчиков</p>
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+
   </section>
 </template>
 
