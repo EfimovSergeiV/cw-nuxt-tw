8e3ea37c15f7f786556af607764ac28e0ccf7012
anon <anon@debian-BULLSEYE-live-builder-AMD64>
2022-09-30 11:50:27 +0300

    dev

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <h1>THIS IS HEADER</h1>
+  <div class="mx-auto max-w-7xl px-4 lg:max-w-7xl lg:px-8 py-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 items-center justify-items-stretch">
+      <div class="grid justify-items-center md:justify-items-start">
+        <img src="blue-svar.png" width="320" />
+      </div>
+      <div class="group">
+
+        <label class="relative block">
+          
+          <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+            <div class="mdi mdi-24px mdi-store-search text-slate-800"></div>
+          </span>
+          <input class="text-gray-900 placeholder:text-slate-800 block bg-white w-full border border-slate-300 rounded-sm py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-800 focus:ring-sky-800 sm:text-sm" placeholder="Поиск по товарам..." type="text" name="search"/>
+        
+        
+        
+        
+        </label>
+
+
+          <div class="absolute z-40 w-full left-0 invisible group-focus-within:visible hover:visible group-hover:opacity-100">
+            <div class="container mt-4">
+              <div class="mx-auto py-1 pb-1 px-4 lg:max-w-7xl lg:px-8">
+                <div class="search-result bg-white border-gray-200 shadow-sm border dark:bg-gray-800 dark:border-gray-600">
+                  
+                  <div class="p-3">
+                    <p v-for="i in 20" :key="i" class="text-gray-500 dark:text-gray-300">Results:</p>
+                  </div>
+                  
+
+
+                </div>
+              </div>
+            </div>
+          </div>
+
+      </div>
+      
+    </div>
   </div>
 </template>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,22 +4,7 @@
         <nav class="">
       
           <div class="bg-white border-gray-200 border dark:border-gray-700 dark:bg-gray-800">
-            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-8 md:px-8 lg:px-8 py-2.5">
-                <nuxt-link to="/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
-                    <span class="self-center text-md font-semibold whitespace-nowrap dark:text-white">title</span>
-                </nuxt-link>
-                <button data-collapse-toggle="mega-menu-full" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
-                        <span class="sr-only">Open main menu</span>
-                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                </button>
-
-                <!-- <div class="static ...">
-                  <p>Static parent</p>
-                  <div class="absolute bottom-0 left-0 ...">
-                    <p>Absolute child</p>
-                  </div>
-                </div> -->
+            <div class="grid justify-items-end mx-auto max-w-screen-xl px-8 md:px-8 lg:px-8 py-2.5">
 
                 <div id="mega-menu-full" class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1">
                     <ul class="flex flex-col mt-4 text-sm font-medium md:flex-row md:space-x-8 md:mt-0">
