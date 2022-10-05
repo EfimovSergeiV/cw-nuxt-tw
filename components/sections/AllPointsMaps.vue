bc4068b5000b9cad35b7f61d76924893277c37ef
anon <efimovsergeiv@gmail.com>
2022-10-05 12:22:04 +0300

    dev:

--- components/sections/AllPointsMaps.vue
+++ components/sections/AllPointsMaps.vue
@@ -16,51 +16,41 @@
 
       <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div class="bg-white rounded-sm border dark:border-gray-700 dark:bg-gray-800 shadow-md" v-for="shop in shops" :key="shops.id">
+
+          <div class="block">
+            <div class="flex flex-col bg-gray-400 relative overflow-hidden">
+              <div class="flex justify-end bg-gray-400 overflow-hidden">
+                <img class="object-cover hover:opacity-75 transition ease-in-out duration-150 hover:scale-110 transform" src="140-140.png" alt="">
+              </div>
+
+              <div class="absolute">
+                <div class="text-black mx-3 my-3">
+                  <p class="mdi mdi-content-copy cursor-pointer" @click="copyToBuffer(shop.adress)"> {{ shop.adress }}</p>
+                  <a :href="'tel:' + shop.phone">{{ shop.phone }}</a>
+                  <p>{{ shop.wday }}</p>
+                  <p>{{ shop.wend }}</p>            
+                </div>
+              </div>
+            </div>
+
           
-          <!-- <div class="">
-            <img src="120-120.jpg" class="absolute"/>
+          
 
-            <div class="">
-              <p class="mdi mdi-content-copy cursor-pointer" @click="copyToBuffer(shop.adress)"> {{ shop.adress }}</p>
-              <a :href="'tel:' + shop.phone">{{ shop.phone }}</a>
-              <p>{{ shop.wday }}</p>
-              <p>{{ shop.wend }}</p>            
-            </div>
-
-          </div> -->
-
-
-
-          <div class="flex flex-col bg-gray-400 relative overflow-hidden">
-            <a href="#" class="block bg-gray-400 overflow-hidden">
-              <img class="object-cover hover:opacity-75 transition ease-in-out duration-150 hover:scale-110 transform" src="120-120.jpg" alt="">
-            </a>
-
-          <div class="absolute">
-            <div class="p-2 text-black">
-              <p class="mdi mdi-content-copy cursor-pointer" @click="copyToBuffer(shop.adress)"> {{ shop.adress }}</p>
-              <a :href="'tel:' + shop.phone">{{ shop.phone }}</a>
-              <p>{{ shop.wday }}</p>
-              <p>{{ shop.wend }}</p>            
-            </div>
-          </div>            
           </div>
+
 
 
         </div>
       </div>
 
 
-
-      <div class="mt-4" style="width: 100%">
+      <div class="mt-4">
         <iframe 
-          src="https://www.google.com/maps/d/embed?mid=1v3MRuieVKECJQz5HZ1i51CJwv0W4-yA&ehbc=2E312F" 
-          border-radius: 0px; 
+          src="https://yandex.ru/map-widget/v1/?um=constructor%3A1dc40cde3020c34cd82fe8d78396fb83f228f0cf7079487c92e3f4bd2f161f34&amp;source=constructor" 
           width="100%" 
-          height="380" 
-          loading="lazy">
+          height="400" 
+          frameborder="0">
         </iframe>
-
       </div>
 
 
