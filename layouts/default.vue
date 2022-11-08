ee82aa8d57d1d4ba6f810ef3ce9970034e595e04
anon <efimovsergeiv@gmail.com>
2022-11-08 17:09:55 +0300

    dev:

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="bg-gray-100 bg-gradient-to-r dark:from-gray-900 dark:to-gray-800 text-gray-700 dark:text-gray-300">
+  <div class="bg-gray-100 bg-gradient-to-r dark:from-gray-900 dark:to-gray-800 text-gray-700 dark:text-gray-300 footer-bg">
     
+    <div class="footer-bg">
       <div class="container mx-auto">
         
         <transition name="fade" mode="out-in">
           <Nuxt class="" />
         </transition>
 
-      </div>
+      </div>      
     </div>
+
+  </div>
 
 </template>
   
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen">
+  <div class="mmmin-h-screen">
 
     <Header />
     <Navbar :cts="cts" />
