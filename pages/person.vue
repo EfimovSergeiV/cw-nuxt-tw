0501277bcab4ca824ab83be60219bad276b64e0a
anon <efimovsergeiv@gmail.com>
2022-12-28 11:13:46 +0300

    add:

--- pages/person.vue
+++ pages/person.vue
@@ -8,76 +8,112 @@
 
     <div class="mx-auto py-2 px-4 lg:max-w-7xl lg:px-8">
 
+      <div class="grid md:grid-cols-2 gap-4">
 
-      <div class="bg-white dark:bg-gray-800 px-4 py-4 mb-4 border border-gray-300 dark:border-gray-700">
-        
-        <div class="grid gap-4">
-          <div class="flex gap-8 text-sm">
 
-            <div class="grid gap-4">
-              <button to="#" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">История заказов</button>
-              <button to="#" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">Избранное</button>
-              <button to="#" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">Товары в сравнении</button>
-              <button to="#" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">Персональные данные</button>
+        <div class="bg-white dark:bg-gray-800 px-4 py-4 mb-4 border border-gray-300 dark:border-gray-700">
+          <div class="grid gap-2">
+
+            <div class="flex">
+              <p class="text-sm">История заказов</p>
             </div>
-            
-            <div class="">
 
-              <div class="">
-                <label for="message" class="block mt-2 mb-1 text-xs font-medium text-gray-900 dark:text-gray-400">Имя</label>
-                <div class="relative">
-                  <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                    <p class="mdi mdi-account"></p>
+            <div class="overflow-y-scroll h-80">
+              <div class="grid grid-cols-1 gap-2">
+                
+                <div v-for="i in 15" :key="i" class="">
+                  <div class="bg-white dark:bg-gray-700 rounded-sm border dark:border-gray-600 p-1">
+                    <div class="">
+                      <p class="text-sm">PSK0000{{ i }}</p>
+                    </div>
+                    <div class="">
+                      <button to="#" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 text-sm">Отменить</button>
+                    </div>
+                    
                   </div>
-                  <input type="text" id="person" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Иван Иванов">
+                  
                 </div>
+
               </div>
-
-              <div class="">
-                <label for="message" class="block mt-2 mb-1 text-xs font-medium text-gray-900 dark:text-gray-400">Электронная почта</label>
-                <div class="relative">
-                  <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                    <p class="mdi mdi-email"></p>
-                  </div>
-                  <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@domen.com">
-                </div>
-              </div>
-
-              <div class="">
-                <label for="message" class="block mt-2 mb-1 text-xs font-medium text-gray-900 dark:text-gray-400">Номер телефона</label>
-                <div class="relative">
-                  <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                    <p class="mdi mdi-phone"></p>
-                  </div>
-                  <input  type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="+7 (987) 654 32 10">
-                </div>               
-              </div>
-
-            </div>
-
-
-          </div>
-
-          <div class="flex items-center justify-end gap-4">
-            <div class="">
-              <button class="w-full relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-400 to-blue-800 group-hover:from-blue-400 group-hover:to-blue-800 hover:text-gray-100 dark:text-gray-300 hover:dark:text-gray-100 focus:ring-1 focus:outline-none focus:ring-cyan-200 dark:focus:ring-blue-700">
-                <span class="px-8 py-1 relative transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                  Сохранить
-                </span>
-              </button>
-            </div>
-            <div class="">
-              <button to="#" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 text-sm">Удалить профиль</button>
             </div>
 
           </div>
         </div>
 
+
+
+        <div class="bg-white dark:bg-gray-800 px-4 py-4 mb-4 border border-gray-300 dark:border-gray-700">
+          <div class="grid gap-4 h-full">
+            <div class="flex">
+              <p class="text-sm">Персональные данные</p>
+            </div>
+
+            <div class="grid content-between">
+              <div class="">
+                <div class="">
+                  <label for="message" class="block mt-2 mb-1 text-xs font-medium text-gray-900 dark:text-gray-400">Имя</label>
+                  <div class="relative">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                      <p class="mdi mdi-account"></p>
+                    </div>
+                    <input type="text" id="person" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Иван Иванов">
+                  </div>
+                </div>
+
+                <div class="">
+                  <label for="message" class="block mt-2 mb-1 text-xs font-medium text-gray-900 dark:text-gray-400">Электронная почта</label>
+                  <div class="relative">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                      <p class="mdi mdi-email"></p>
+                    </div>
+                    <input type="text" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@domen.com">
+                  </div>
+                </div>
+
+                <div class="">
+                  <label for="message" class="block mt-2 mb-1 text-xs font-medium text-gray-900 dark:text-gray-400">Номер телефона</label>
+                  <div class="relative">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                      <p class="mdi mdi-phone"></p>
+                    </div>
+                    <input  type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="+7 (987) 654 32 10">
+                  </div>               
+                </div>
+              </div>
+
+              <div class="flex items-center justify-end gap-4 h-full my-2">
+                <div class="">
+                  <button class="w-full relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-400 to-blue-800 group-hover:from-blue-400 group-hover:to-blue-800 hover:text-gray-100 dark:text-gray-300 hover:dark:text-gray-100 focus:ring-1 focus:outline-none focus:ring-cyan-200 dark:focus:ring-blue-700">
+                    <span class="px-8 py-2 relative transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                      Сохранить
+                    </span>
+                  </button>
+                </div>
+                <div class="">
+                  <button to="#" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 text-sm">Удалить профиль</button>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+        
+        <div class="bg-white dark:bg-gray-800 px-4 py-4 mb-4 border border-gray-300 dark:border-gray-700">
+          <div class="flex">
+            <p class="text-sm">Товары в избранном</p>
+          </div>
+        </div>
+        <div class="bg-white dark:bg-gray-800 px-4 py-4 mb-4 border border-gray-300 dark:border-gray-700">
+          <div class="flex">
+            <p class="text-sm">Товары в сравнении</p>
+          </div>
+        </div>
       </div>
 
 
-      <div class="bg-white dark:bg-gray-800 px-12 py-4 border border-gray-300 dark:border-gray-700">
-
+      <!-- <div class="bg-white dark:bg-gray-800 px-12 py-4 border border-gray-300 dark:border-gray-700">
 
         <div class="flex gap-4 my-4">
           <button @click="createMainCookie()" class="bg-green-500 rounded-sm px-5 py-1 shadow-md font-bold hover:bg-green-600 text-gray-100">Create State</button>
@@ -92,7 +128,6 @@
           <button @click="editCookie()" class="bg-yellow-500 rounded-sm px-4 py-1 shadow-md font-bold hover:bg-yellow-600 text-gray-100">Edit Cookie</button>
           <button @click="removeCookie()" class="bg-red-500 rounded-sm px-4 py-1 shadow-md font-bold hover:bg-red-600 text-gray-100">Remove Cookie</button>
         </div>
-        
 
       </div>
 
@@ -104,7 +139,7 @@
           <p class="text-xs">allCookie : {{ allCookie }}</p>
           <p class="text-xs">clientName : {{ clientName }}</p>
         </div>
-      </div>
+      </div> -->
 
     </div>
 
@@ -137,6 +172,7 @@ import Order from '~/components/sections/Order.vue'
     data() {
       return {
         stat: false,
+        activeTab: 'orders',
         allCookie: null,
       }
     },
