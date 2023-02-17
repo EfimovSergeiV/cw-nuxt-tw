665ffb6fdab8b687060fdcd09b0ddc90719b9389
anon <efimovsergeiv@gmail.com>
2023-02-17 14:58:58 +0300

    add:

--- components/sections/Reviews.vue
+++ components/sections/Reviews.vue
@@ -93,65 +93,7 @@
     },
     data() {
       return {
-        reviewss: [
-          {
-            id: 1,
-            name: 'Правильная сварка - Сварог',
-            link: { name: `products`, query: { brnd: 9, page: 1 } },
-            video:
-              'https://www.youtube.com/embed/SakTd5qGn24?autoplay=0&amp;loop=1&amp;&amp;playlist=SakTd5qGn24',
-          },
-          {
-            id: 2,
-            name: 'PRO TIG 315P AC/DC - Сварог',
-            link: { name: 'product-id', params: { id: 1035 } },
-            video:
-              'https://www.youtube.com/embed/600W5mJjTco?autoplay=0&amp;loop=1&amp;&amp;playlist=600W5mJjTco',
-          },
-          {
-            id: 3,
-            name: 'REAL SMART MIG 200 - Сварог',
-            link: { name: 'product-id', params: { id: 1039 } },
-            video:
-              'https://www.youtube.com/embed/TV4Ku9q9M0Y?autoplay=0&amp;loop=1&amp;&amp;playlist=TV4Ku9q9M0Y',
-          },
-          {
-            id: 4,
-            name: 'REAL SMART ARC 160 - Сварог',
-            link: { name: 'product-id', params: { id: 1058 } },
-            video:
-              'https://www.youtube.com/embed/owyfZ-ATr9Y?autoplay=0&amp;loop=1&amp;&amp;playlist=owyfZ-ATr9Y',
-          },
 
-          {
-            id: 5,
-            name: 'REAL SMART ARC 160 - Сварог',
-            link: { name: 'product-id', params: { id: 1058 } },
-            video:
-              'https://www.youtube.com/embed/RMXycPSo6WE',
-          },
-          {
-            id: 6,
-            name: 'REAL SMART ARC 160 - Сварог',
-            link: { name: 'product-id', params: { id: 1058 } },
-            video:
-              'https://www.youtube.com/embed/LIRjPeQSP_w',
-          },
-          {
-            id: 7,
-            name: 'REAL SMART ARC 160 - Сварог',
-            link: { name: 'product-id', params: { id: 1058 } },
-            video:
-              'https://www.youtube.com/embed/rg7rONVwq6A',
-          },
-          {
-            id: 8,
-            name: 'REAL SMART ARC 160 - Сварог',
-            link: { name: 'product-id', params: { id: 1058 } },
-            video:
-              'https://www.youtube.com/embed/k1f8S3K3m5w',
-          },
-        ],
       }
     },
   }
