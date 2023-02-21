ac9cf2c710faca7a326a10225d83ed10fe9b7c25
anon <efimovsergeiv@gmail.com>
2023-02-21 14:25:10 +0300

    add:

--- components/sections/MainWelderSection.vue
+++ components/sections/MainWelderSection.vue
@@ -7,18 +7,18 @@
         <div class="">
           <div class="bg-white dark:bg-gray-800 h-full rounded-sm border dark:border-gray-600 border-gray-200 shadow-sm">
             <div class="relative">
-              <img src="main-mw-banner.png" class="" />
-              <div class="absolute h-full w-full top-0 bg-gray-800/50">
+              <img src="main-mw-banner.png" class="rounded-sm" />
+              <div class="absolute h-full w-full top-0 bg-gray-800/50 rounded-sm">
                 <div class="flex items-center h-full px-2">
                   
-                  <div class="">
+                  <div class="grid grid-cols-1 gap-2">
 
                     <div class="flex gap-2">
-                      <p class="text-2xl font-semibold my-4 uppercase shadow-2xl">Становись профессионалом</p>
-                      <p class="text-2xl font-bold my-4 uppercase shadow-2xl text-white">с нами!</p>
+                      <p class="text-2xl font-semibold uppercase shadow-2xl text-gray-200">Становись профессионалом</p>
+                      <p class="text-2xl font-bold uppercase shadow-2xl text-white ">с нами!</p>
                     </div>
                     
-                    <nuxt-link :to="{ name: 'cts'}" class="lowercase">Перейти в каталог</nuxt-link>
+                    <nuxt-link :to="{ name: 'cts'}" class="lowercase text-gray-100">Перейти в каталог</nuxt-link>
                   </div>
                   
                 </div>
@@ -34,13 +34,13 @@
           <div class="bg-white dark:bg-gray-800 h-full rounded-sm border dark:border-gray-600 border-gray-200 shadow-sm">
             <div class="relative">
               <img src="first-mw-banner.jpg" class="" />
-              <div class="absolute h-full w-full top-0 bg-gray-800/50">
+              <div class="absolute h-full w-full top-0 bg-gray-800/50 rounded-sm">
                 <div class="flex items-center h-full px-2">
                   <div class="">
                     <div class="flex gap-2">
                       <div class="px-2 grid grid-cols-1 gap-4">
                         <p class=" text-lg font-bold text-white">Ручная дуговая сварка</p>
-                        <nuxt-link :to="{ name: 'cts'}" class="uppercase text-xs font-semibold underline">Перейти в категорию</nuxt-link>
+                        <nuxt-link :to="{ name: 'prods', query: { ct: 8 } }" class="uppercase text-xs font-semibold underline text-gray-100">Перейти в категорию</nuxt-link>
                       </div>
                     </div>
                   </div>
@@ -52,14 +52,14 @@
           <div class="relative">
             <div class="bg-white dark:bg-gray-800 h-full rounded-sm border dark:border-gray-600 border-gray-200 shadow-sm">
               <div class="relative">
-                <img src="two-mw-banner.jpg" class="" />
-                <div class="absolute h-full w-full top-0 bg-gray-800/50">
+                <img src="two-mw-banner.jpg" class=""/>
+                <div class="absolute h-full w-full top-0 bg-gray-800/50 rounded-sm">
                   <div class="flex items-center h-full px-2">
                     <div class="">
                       <div class="flex gap-2">
                         <div class="px-2 grid grid-cols-1 gap-4">
                           <p class=" text-lg font-bold text-white">Полуавтоматическая сварка</p>
-                          <nuxt-link :to="{ name: 'cts'}" class="uppercase text-xs font-semibold underline">Перейти в категорию</nuxt-link>
+                          <nuxt-link :to="{ name: 'prods', query: { ct: 9 } }" class="uppercase text-xs font-semibold underline text-gray-100">Перейти в категорию</nuxt-link>
                         </div>
                       </div>
                     </div>
