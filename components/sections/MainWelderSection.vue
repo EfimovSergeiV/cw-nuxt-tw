af04da528f39ac1d29c0ecd4d7bb58fa89153955
anon <efimovsergeiv@gmail.com>
2023-03-03 14:01:36 +0300

    add:

--- components/sections/MainWelderSection.vue
+++ components/sections/MainWelderSection.vue
@@ -8,17 +8,17 @@
           <div class="bg-white dark:bg-gray-800 h-full rounded-sm border border-gray-100 dark:border-gray-700 shadow-sm">
             <div class="relative">
               <img src="main-mw-banner.png" class="rounded-sm h-full w-full" fluid />
-              <div class="absolute h-full w-full top-0 bg-gray-800/80 rounded-sm">
+              <div class="absolute h-full w-full top-0 bg-gray-200/80 dark:bg-gray-800/80 rounded-sm">
                 <div class="flex items-center h-full px-4">
                   
                   <div class="grid grid-cols-1 gap-2">
 
                     <div class="flex gap-2">
                       <p class="text-base md:text-xl font-semibold uppercase shadow-2xl text-red-500">Становись профессионалом</p>
-                      <p class="text-base md:text-xl font-bold uppercase shadow-2xl text-gray-100 ">с нами!</p>
+                      <p class="text-base md:text-xl font-bold uppercase shadow-2xl text-gray-700 dark:text-gray-100 ">с нами!</p>
                     </div>
                     
-                    <nuxt-link :to="{ name: 'cts'}" class="lowercase text-gray-100">Перейти в каталог</nuxt-link>
+                    <nuxt-link :to="{ name: 'cts'}" class="text-gray-700 hover:text-gray-900 dark:text-gray-100 transition-all">Перейти в каталог</nuxt-link>
                   </div>
                   
                 </div>
@@ -30,17 +30,21 @@
         </div>
 
 
-        <div class="grid grid-cols-2 gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div class="bg-white dark:bg-gray-800 h-full rounded-sm border border-gray-100 dark:border-gray-700 shadow-sm">
             <div class="relative h-full w-full">
-              <img src="first-mw-banner.jpg" class="overflow-hidden rounded-sm h-full w-full" />
-              <div class="absolute h-full w-full top-0 bg-gray-800/80 rounded-sm">
+              
+              <div class="overflow-hidden min-h-[150px] h-full w-full">
+                <img src="first-mw-banner.jpg" class="overflow-hidden rounded-sm hidden md:block h-full w-full " />
+              </div>
+              
+              <div class="absolute h-full w-full top-0 bg-gray-200/80 dark:bg-gray-800/80 rounded-sm">
                 
                 <div class="flex items-center h-full px-2">
                   <div class="">
                     
                     <div class="my-2">
-                      <nuxt-link to="#" class="text-sm font-semibold">Сварочное оборудование</nuxt-link>
+                      <nuxt-link :to="{ name: 'prods', query: { 'ct': 1 }}" class="text-sm font-semibold text-gray-900 dark:text-gray-100 transition-all">Сварочное оборудование</nuxt-link>
                     </div>
                     
 
@@ -55,9 +59,9 @@
                           </div>
                         </div>
                         <div class="grid grid-cols-1 gap-2">
-                          <nuxt-link to="#" class="text-xs">Ручная дуговая сварка</nuxt-link>
-                          <nuxt-link to="#" class="text-xs">Полуавтоматическая сварка</nuxt-link>
-                          <nuxt-link to="#" class="text-xs">Аргонодуговая сварка</nuxt-link>
+                          <nuxt-link :to="{ name: 'prods', query: { 'ct': 8 }}" class="text-xs text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 transition-all">MMA сварка</nuxt-link>
+                          <nuxt-link :to="{ name: 'prods', query: { 'ct': 9 }}" class="text-xs text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 transition-all">MIG/MAG сварка</nuxt-link>
+                          <nuxt-link :to="{ name: 'prods', query: { 'ct': 11 }}" class="text-xs text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 transition-all">TIG сварка</nuxt-link>
                         </div>
                       </div>
                     </div>                     
@@ -71,15 +75,19 @@
           <div class="relative">
             <div class="bg-white dark:bg-gray-800 h-full rounded-sm border border-gray-100 dark:border-gray-700 shadow-sm">
               <div class="relative h-full w-full">
-                <img src="two-mw-banner.jpg" class="overflow-hidden rounded-sm h-full w-full"/>
-                <div class="absolute h-full w-full top-0 bg-gray-800/80 rounded-sm">
+
+                <div class="overflow-hidden min-h-[150px] h-full w-full">
+                  <img src="two-mw-banner.jpg" class="rounded-sm hidden md:block h-full w-full" />
+                </div>
+
+                <div class="absolute h-full w-full top-0 bg-gray-200/80 dark:bg-gray-800/80 rounded-sm">
 
                   
                   <div class="flex items-center h-full px-2">
                     <div class="">
                       
                       <div class="my-2">
-                        <nuxt-link to="#" class="text-sm font-semibold">Сопутствующие товары</nuxt-link>
+                        <p class="text-sm font-semibold text-gray-900 dark:text-gray-100 transition-all">Сопутствующие товары</p>
                       </div>
                       
 
@@ -90,17 +98,20 @@
                         <div class="flex items-center gap-4 my-2">
                           <div class=" w-20 h-20">
                             <div class="bg-white rounded-md w-full h-full">
-                              <img src="icons/mask-sm.png" class="rounded-md p-2" />
+                              <div class=" min-h-[150px]">
+                                <img src="icons/mask-sm.png" class="rounded-md p-2" />
+                              </div>
+                              
                             </div>
                           </div>
                           <div class="grid grid-cols-1 gap-2">
-                            <nuxt-link to="#" class="text-xs">Сварочные маски</nuxt-link>
+                            <nuxt-link :to="{ name: 'prods', query: { 'ct': 77 }}" class="text-xs text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 transition-all">Сварочные маски</nuxt-link>
                             <div class="flex gap-1">
-                              <nuxt-link to="#" class="text-xs">Электроды</nuxt-link>
+                              <nuxt-link :to="{ name: 'prods', query: { 'ct': 14 }}" class="text-xs text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 transition-all">Электроды</nuxt-link>
                               <p class="text-xs"> и </p>
-                              <nuxt-link to="#" class="text-xs">проволока</nuxt-link>
+                              <nuxt-link :to="{ name: 'prods', query: { 'ct': 18 }}" class="text-xs text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 transition-all">проволока</nuxt-link>
                             </div>
-                            <nuxt-link to="#" class="text-xs">Химия для сварки</nuxt-link>
+                            <nuxt-link :to="{ name: 'prods', query: { 'ct': 5 }}" class="text-xs text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 transition-all">Химия для сварки</nuxt-link>
                           </div>
                         </div>
                       </div>                     
