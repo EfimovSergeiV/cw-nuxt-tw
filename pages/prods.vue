9b9092e39861877c3e4d77285426ca033e1e6332
Sergei Efimov <efimovsergeiv@gmail.com>
2023-03-10 14:21:25 +0300

    add:

--- pages/prods.vue
+++ pages/prods.vue
@@ -4,9 +4,33 @@
     <Header />
     <Navbar :cts="cts" />
 
-    <div class="mx-auto px-4 lg:max-w-7xl lg:px-8">
-      <div id="cat-title" class="flex justify-end mb-1">
-        <p class="text-2xl">{{ response.meta.title }}</p>
+    <div class="mx-auto px-4 lg:max-w-7xl lg:px-8 mb-4">
+
+      <div class="">
+
+        <div id="cat-title" class="flex items-center justify-end mb-2">
+          <p class="text-2xl">{{ response.meta.title }}</p>
+        </div>
+
+        <div v-if="response.meta.inserted" class="flex items-center justify-end">
+          <div id="" class="">
+
+            <div class="flex flex-wrap gap-2 justify-end max-w-[700px]">
+              <div v-for="inserted in response.meta.inserted" :key="inserted.id" class="">
+                <div class="bg-white dark:bg-gray-800 px-4 rounded-xl border hover:border-gray-300 dark:border-gray-700 border-gray-200 hover:dark:border-gray-600 transition-all shadow-md">
+                  <nuxt-link :to="{ name: 'prods', query: { ct: inserted.id}}" class="text-xs">{{ inserted.name }}</nuxt-link>
+                </div>
+              </div>          
+            </div>
+
+          </div>
+        </div>
+        <div v-else></div>
+
+
+
+
+
       </div>
     </div>
 
@@ -14,19 +38,7 @@
 
     <Breadcrumbs :breadcrumbs="breadcrumbs" />
 
-    <div v-if="response.meta.inserted" class="mx-auto px-4 lg:max-w-7xl lg:px-8">
-      <div id="" class="my-4">
 
-
-        <div class="flex flex-wrap gap-8 items-center justify-center">
-          <div v-for="inserted in response.meta.inserted" :key="inserted.id" class="h-full">
-            <nuxt-link :to="{ name: 'prods', query: { ct: inserted.id}}" class="text-sm">{{ inserted.name }}</nuxt-link>
-          </div>          
-        </div>
-
-
-      </div>
-    </div>
 
     <Filters :props="props" :brands="brands" :response="response" />
     <Products :products="response.results" class="" />
