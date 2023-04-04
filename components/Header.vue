56ef7c126fe95989a386fcccafdc6cd8e908b6e4
Sergei Efimov <efimovsergeiv@gmail.com>
2023-04-04 16:30:12 +0300

    add:

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="mx-auto max-w-7xl px-4 lg:max-w-7xl lg:px-8 pt-2">
 
-
-    <div class="relative">
+    <div class=" relative">
       <div id="main-info" class="flex justify-between items-start gap-2 md:gap-6 mt-2">
- 
+
         <div class="">
           <div id="color-mode" class="flex gap-1">
 
@@ -112,128 +111,63 @@
     </div>
 
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
+    <div class="relative">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
 
-      <div class="">
-        <div class="flex justify-center md:justify-start">
-          <nuxt-link to="/">
-            <img src="../assets/images/blue-svar.webp" alt="glsvar-logo" class=" h-18 md:h-16 select-none" />
-          </nuxt-link>
+        <div class="">
+          <div class="flex justify-center md:justify-start">
+            <nuxt-link to="/">
+              <img src="../assets/images/blue-svar.webp" alt="glsvar-logo" class=" h-18 md:h-16 select-none" />
+            </nuxt-link>
+          </div>
         </div>
-      </div>
 
-      <div class="relative">
-        <label class="group">
-          <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-            <div class="mdi mdi-magnify text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"></div>
-          </span>
-          <input v-model="search" class="text-gray-700 w-full hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 placeholder:text-slate-500 focus:ring-0 peer block bg-white dark:bg-gray-800 dark:placeholder:text-slate-400 border dark:border-slate-600 border-slate-300 rounded-sm py-2 pl-7 pr-3 shadow-sm focus:outline-none focus:border-sky-800 focus:ring-sky-800 sm:text-sm" placeholder="Поиск по товарам" type="text" name="search"/>
-          <div class="absolute z-50 w-full invisible group-focus-within:visible hover:visible group-hover:opacity-100">
-            <div class="container">
-              <div class="">
-                <div class="search-result bg-white border-gray-200 shadow-sm border dark:bg-gray-800 dark:border-gray-600">
-                  <div v-if="searchResult.length > 0" class="h-96 overflow-y-auto">
-                    <div v-for="searchresult in searchResult" :key="searchresult.id" class="px-2 py-1">
-                      <div class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 py-1 px-1">
-                        <nuxt-link :to="{ name: 'product-id', params: { id: searchresult.id } }" class="">
-                          <div class="flex items-center justify-between gap-1">
-                            <div class="flex items-center gap-2">
-                              <img v-if="searchresult.preview_image" :src="searchresult.preview_image" class=" rounded-sm h-14 bg-white p-1" />
-                              <p class="text-sm">{{ searchresult.name }}</p>
-                            </div>
-                            <div class="">
-                              <div class="flex items-center">
-                                <div v-if="searchresult.only_price >= 0" class="text-right">
-                                  <p v-if="searchresult.only_price > 0" class="text-sm">{{ searchresult.only_price.toLocaleString() }} руб.</p>
-                                  <p v-else class="text-xs text-center">По запросу</p>
+        <div class="relative">
+          <label class="group">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+              <div class="mdi mdi-magnify text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"></div>
+            </span>
+            <input v-model="search" class="text-gray-700 w-full hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 placeholder:text-slate-500 focus:ring-0 peer block bg-white dark:bg-gray-800 dark:placeholder:text-slate-400 border dark:border-slate-600 border-slate-300 rounded-sm py-2 pl-7 pr-3 shadow-sm focus:outline-none focus:border-sky-800 focus:ring-sky-800 sm:text-sm" placeholder="Поиск по товарам" type="text" name="search"/>
+            <div class="absolute z-50 w-full invisible group-focus-within:visible hover:visible group-hover:opacity-100">
+              <div class="container">
+                <div class="">
+                  <div class="search-result bg-white border-gray-200 shadow-sm border dark:bg-gray-800 dark:border-gray-600">
+                    <div v-if="searchResult.length > 0" class="h-96 overflow-y-auto">
+                      <div v-for="searchresult in searchResult" :key="searchresult.id" class="px-2 py-1">
+                        <div class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 py-1 px-1">
+                          <nuxt-link :to="{ name: 'product-id', params: { id: searchresult.id } }" class="">
+                            <div class="flex items-center justify-between gap-1">
+                              <div class="flex items-center gap-2">
+                                <img v-if="searchresult.preview_image" :src="searchresult.preview_image" class=" rounded-sm h-14 bg-white p-1" />
+                                <p class="text-sm">{{ searchresult.name }}</p>
+                              </div>
+                              <div class="">
+                                <div class="flex items-center">
+                                  <div v-if="searchresult.only_price >= 0" class="text-right">
+                                    <p v-if="searchresult.only_price > 0" class="text-sm">{{ searchresult.only_price.toLocaleString() }} руб.</p>
+                                    <p v-else class="text-xs text-center">По запросу</p>
+                                  </div>
                                 </div>
                               </div>
                             </div>
-                          </div>
-                        </nuxt-link>                      
+                          </nuxt-link>                      
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div v-else class="px-4 py-4">
-                    <p class="text-sm">Начните вводить запрос</p>
+                    <div v-else class="px-4 py-4">
+                      <p class="text-sm">Начните вводить запрос</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </label>
+          </label>
+        </div>
+
       </div>
 
     </div>
 
-
-
-    <!-- <div class="grid grid-cols-1 md:grid-cols-2 items-center">
-
-      <div class="">
-
-      </div>
-
-      
-      <div class="group my-4">
-
-        <label class="relative block">
-          
-          <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-            <div class="mdi mdi-magnify text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100"></div>
-          </span>
-          <input v-model="search" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 placeholder:text-slate-500 focus:ring-0 peer block bg-white dark:bg-gray-800 dark:placeholder:text-slate-600 w-full border dark:border-slate-600 border-slate-300 rounded-sm py-2 pl-7 pr-3 shadow-sm focus:outline-none focus:border-sky-800 focus:ring-sky-800 sm:text-sm" placeholder="Поиск по товарам" type="text" name="search"/>
-
-
-          <div class="absolute z-40 w-full invisible group-focus-within:visible hover:visible group-hover:opacity-100">
-            <div class="container">
-              <div class="">
-                <div class="search-result bg-white border-gray-200 shadow-sm border dark:bg-gray-800 dark:border-gray-600">
-                  
-                  <div v-if="searchResult.length > 0" class="h-96 overflow-y-auto">
-
-
-                    <div v-for="searchresult in searchResult" :key="searchresult.id" class="px-2 py-1">
-                      <div class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 py-1 px-1">
-                        <nuxt-link :to="{ name: 'product-id', params: { id: searchresult.id } }" class="">
-                          
-
-                          <div class="flex items-center justify-between gap-1">
-                            <div class="flex items-center gap-2">
-                              <img v-if="searchresult.preview_image" :src="searchresult.preview_image" class=" rounded-sm h-14 bg-white p-1" />
-                              <p class="text-sm">{{ searchresult.name }}</p>
-                            </div>
-                            <div class="">
-                              <div class="flex items-center">
-                                <div v-if="searchresult.only_price >= 0" class="text-right">
-                                  <p v-if="searchresult.only_price > 0" class="text-sm">{{ searchresult.only_price.toLocaleString() }} руб.</p>
-                                  <p v-else class="text-xs text-center">По запросу</p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-
-                          
-                        </nuxt-link>                      
-                      </div>
-                    </div>
-
-
-                  </div>
-
-                  <div v-else class="px-4 py-4">
-                    <p class="text-sm">Начните вводить запрос</p>
-                  </div>
-
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </label>
-
-      </div>
-    </div> -->
 
   </div>
 </template>
