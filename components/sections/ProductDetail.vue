e284f9e558ace111bd2f2d0a80fdbc8c9424b0f4
Sergei Efimov <efimovsergeiv@gmail.com>
2023-04-07 11:13:09 +0300

    add:

--- components/sections/ProductDetail.vue
+++ components/sections/ProductDetail.vue
@@ -10,7 +10,7 @@
               <div class="flex gap-4">
 
 
-                <div class="grid grid-cols-1 gap-2 px-2 py-2">
+                <div class="px-2 py-2">
                   <div v-for="image in product.prod_img.slice(0, 4)" :key="image.id" class="w-[80px] h-[80px] rounded-md bg-white flex items-center justify-center">
                     <img :src="image.image" class="p-2" />
                   </div>
