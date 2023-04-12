b061cf115cc4461169d679c8db79381c8397a44f
Sergei Efimov <efimovsergeiv@gmail.com>
2023-04-12 10:08:51 +0300

    add:

--- components/sections/Categories.vue
+++ components/sections/Categories.vue
@@ -4,7 +4,7 @@
 
     <div class="mx-auto px-4 lg:max-w-7xl lg:px-8">
 
-      <div class="bg-white border-gray-200 shadow-sm border dark:bg-gray-800 dark:border-gray-700 p-2">
+      <div class="bg-white border-gray-200 shadow-sm border dark:bg-gray-800 dark:border-gray-700 p-2 ">
         <div class="columns-1 md:columns-3 lg:columns-4">
           <div v-for="ct in cts" :key="ct.id" class="break-inside-avoid-column">
             <div class="p-2">
@@ -13,12 +13,12 @@
               </div>
               <div>
                 <ul>
-                  <li v-for="sct in ct.inserted" :key="sct.id" class="inline-block mr-3 my-1 group">
+                  <li v-for="sct in ct.inserted" :key="sct.id" class="inline-block mr-3 my-1 group relative">
                     
                     <nuxt-link v-if="sct.inserted.length === 0" :to="{ name: 'prods', query: {ct: sct.id}}" class="text-gray-700 text-sm hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">{{ sct.name }}</nuxt-link>
                     <button v-else class="text-gray-700 text-sm hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">{{ sct.name }}</button>
 
-                    <div class="absolute invisible group-hover:opacity-100 group-hover:visible" v-if="sct.inserted.length > 0">
+                    <div class="absolute invisible group-hover:opacity-100 group-hover:visible right-0 z-40" v-if="sct.inserted.length > 0">
                       
                       <div class="bg-white border-gray-200 border rounded-sm dark:border-gray-700 dark:bg-gray-800 p-4">
                         <nuxt-link :to="{ name: 'prods', query: {ct: sct.id}}" class="font-bold">{{ sct.name }}</nuxt-link>
@@ -28,9 +28,9 @@
                           </div>                                 
                         </div>
                       </div>
-
                     </div>
                   </li>
+
                 </ul>
               </div>              
             </div>
